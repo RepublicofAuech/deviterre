9f5063106892abc7a23c2b25fab85a861ac264e4
RepublicofAuech <168240154+RepublicofAuech@users.noreply.github.com>
2024-07-14 16:04:49 +0900

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,7 +1,6 @@
 import { REST, Routes, EmbedBuilder } from 'discord.js';
 import { getRandomStreetViewImage } from './getStreetView.js';
 import { promises as fs } from 'fs';
-import dotenv from 'dotenv';
 
 // 環境変数を読み込む
 dotenv.config();
