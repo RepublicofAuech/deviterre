8432d64d07d79309d04bacfcbacab0233cc1313f
RepublicofAuech <168240154+RepublicofAuech@users.noreply.github.com>
2024-07-14 15:48:26 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -12,13 +12,14 @@ mkdir -p $PUPPETEER_CACHE_DIR
 # Check if Puppeteer is already installed in the cache
 if [ -d "$PUPPETEER_CACHE_DIR/chrome" ]; then
   echo "Puppeteer cache exists, copying to node_modules."
-  cp -r $PUPPETEER_CACHE_DIR/chrome/* node_modules/puppeteer/.local-chromium
+  mkdir -p node_modules/puppeteer/.local-chromium
+  cp -r $PUPPETEER_CACHE_DIR/chrome/* node_modules/puppeteer/.local-chromium/
 else
   echo "Installing Puppeteer."
   npm install puppeteer
   echo "Caching Puppeteer."
   mkdir -p $PUPPETEER_CACHE_DIR/chrome
-  cp -r node_modules/puppeteer/.local-chromium/* $PUPPETEER_CACHE_DIR/chrome
+  cp -r node_modules/puppeteer/.local-chromium/* $PUPPETEER_CACHE_DIR/chrome/
 fi
 
 echo "Build script finished."
